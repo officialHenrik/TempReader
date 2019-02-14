299a5a85eabbd7df73365e8d2798f78b1c2d8473
rpiHg2 <rpi2@example.com>
2019-02-14 19:46:02 +0100

    included if __init__..

--- TempReader.py
+++ TempReader.py
@@ -12,7 +12,7 @@ class TempReader:
       # Enable kernel modules
       os.system('modprobe w1-gpio')
       os.system('modprobe w1-therm')
-      
+
       # Find path to sensor
       base_dir = '/sys/bus/w1/devices/'
       device_folder = glob.glob(base_dir + '28*')[0]
@@ -27,22 +27,26 @@ class TempReader:
 
     # Read temperature in celcius
     def read_temp(self):
-        lines = read_temp_raw()
-        
+        lines = self.read_temp_raw()
+
         # Wait for temperature reading
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = read_temp_raw()
-        
+            lines = self.read_temp_raw()
+
         # Find temperature position
         equals_pos = lines[1].find('t=')
-        
+
         temp_degC = 126000 # default return max temp + 1deg celcius
         # If temperature reading found. Pick it and convert to celcius
         if equals_pos != -1:
             temp_str = lines[1][equals_pos+2:]
             temp_degC = float(temp_str) / 1000.0
-        
+
         # Return temperature in celcius
         return temp_degC
-	
+
+if __name__ == "__main__":
+    tr = TempReader()
+    temp = tr.read_temp()
+    print("{} degC".format(temp))
